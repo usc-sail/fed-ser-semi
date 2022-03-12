84e347f1a003aaabf13d3021af7848f1cc235c22
tiantiaf <tiantiaf@usc.edu>
2022-03-12 00:16:13 +0000

    update data preprocessing code that inlcudes non-iid distribution generation

--- preprocess_data/preprocess_federate_data.py
+++ preprocess_data/preprocess_federate_data.py
@@ -214,7 +214,7 @@ if __name__ == '__main__':
                 if recording_type == 'P' or recording_type == 'R': continue
                 if emotion not in emo_map_dict: continue
                 label, data = emo_map_dict[emotion], data_dict[sentence_file]
-                save_data = np.array(data['data'])[0] if args.feature_type == 'emobase' else np.array(data['data'])[0, 0, :].flatten()
+                save_data = np.array(data['data'])[0] if args.feature_type == 'emobase' or args.feature_type == 'ComParE' else np.array(data['data'])[0, 0, :].flatten()
                 save_data_dict(save_data, label, gender, speaker_id)
         elif data_set_str == 'crema-d':
             
@@ -240,7 +240,7 @@ if __name__ == '__main__':
                 if sentence_file not in data_dict: continue
                 if emotion not in emo_map_dict: continue
                 label, data = emo_map_dict[emotion], data_dict[sentence_file]
-                save_data = np.array(data['data'])[0] if args.feature_type == 'emobase' else np.array(data['data'])[0, 0, :].flatten()
+                save_data = np.array(data['data'])[0] if args.feature_type == 'emobase' or args.feature_type == 'ComParE' else np.array(data['data'])[0, 0, :].flatten()
                 gender = 'M' if demo_df.loc[int(sentence_part[0]), 'Sex'] == 'Male' else 'F'
                 save_data_dict(save_data, label, gender, speaker_id)
         elif data_set_str == 'iemocap':
@@ -266,7 +266,7 @@ if __name__ == '__main__':
                                 gender = sentence_file.split('_')[-1][0]
                                 speaker_id = sentence_file.split('_')[0][:-1] + gender
                                 label, data = line.split('\t')[-2], data_dict[sentence_file]
-                                save_data = np.array(data['data'])[0] if args.feature_type == 'emobase' else np.array(data['data'])[0, 0, :].flatten()
+                                save_data = np.array(data['data'])[0] if args.feature_type == 'emobase' or args.feature_type == 'ComParE' else np.array(data['data'])[0, 0, :].flatten()
                                 
                                 if 'impro' not in line: continue
                                 if label == 'ang' or label == 'neu' or label == 'sad' or label == 'hap' or label == 'exc':
